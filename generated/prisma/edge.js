10639be24cdfb21ca047904f2ada36bf3b302e2a
Martin Heath <mwh82@scarletmail.rutgers.edu>
2025-10-23 18:51:17 -0400

    Clean up documentation

--- generated/prisma/edge.js
+++ generated/prisma/edge.js
@@ -208,7 +208,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
